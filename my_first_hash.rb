3bc1cb33894c2f96b6a27a75d8a6d39cf032a129
Peter Rose <peterroseq1@gmail.com>
2020-03-15 00:19:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,7 @@ end
 #   end
 # end
 
+
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
